8c3798b5f7b2e7a22e872fd0e9aa94e0b3322e27
pytax <na.automatizacao.com.br>
2021-11-18 08:48:57 -0300

    adicionando função para consultar api

---
+++ database.py
@@ -0,0 +1,99 @@
+import sqlite3
+
+class Data_base:
+
+    def __init__(self, name = 'system.db') -> None:        
+        self.name = name
+
+    def connect(self):
+        self.connection = sqlite3.connect(self.name)
+
+    def close_connection(self):
+        try:
+            self.connection.close()
+        except:
+            pass
+    
+    def create_table_company(self):
+        cursor = self.connection.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS Empresa(
+
+            CNPJ TEXT,
+            NOME TEXT,
+            LOGRADOURO TEXT,
+            NUMERO TEXT,
+            COMPLEMENTO TEXT,
+            BAIRRO TEXT,
+            MUNICIPIO TEXT,
+            UF TEXT,
+            CEP TEXT,
+            TELEFONE TEXT,
+            EMAIL TEXT,
+
+            PRIMARY KEY(CNPJ)
+            );
+
+           
+        
+        
+        """)
+    
+    def register_company(self, fullDataSet):
+
+        campos_tabela = ('CNPJ','NOME','LOGRADOURO','NUMERO','COMPLEMENTO','BAIRRO','MUNICIPIO',
+        'UF','CEP','TELEFONE','EMAIL')
+
+        qntd = ("?,?,?,?,?,?,?,?,?,?,?")
+        cursor = self.connection.cursor()
+
+        try:
+            cursor.execute(f"""INSERT INTO Empresas {campos_tabela}
+            VALUES({qntd}),""", fullDataSet)
+            return("OK")
+
+        except:
+            return "Erro"
+
+    def select_all_companies(self):
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute("SELECT * FROM Empresas ORDER BY NOME")
+            empresas = cursor.fetchall()
+            return empresas
+        except:
+            pass
+
+    def delete_companie(self, id):
+
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute(f"DELETE FROM Empresas WHERE CNPJ = '{id}' ")
+
+            self.conection.commit()
+
+            return "Cadastro de empresa excluido com sucesso!"
+
+        except:
+            return "Erro ao Excluir registro!"
+
+    def update_company(self, fullDataSet):
+
+        cursor = self.connection.cursor()
+        cursor.execute(f""" UPDATE Empresas set
+
+            CNPJ = '{fullDataSet[0]}',
+            NOME = '{fullDataSet[1]}',
+            LOGRADOURO = '{fullDataSet[2]}',
+            NUMERO = '{fullDataSet[3]}',
+            COMPLEMENTO = '{fullDataSet[4]}',
+            BAIRRO = '{fullDataSet[5]}',
+            MUNICIPIO = '{fullDataSet[6]}',
+            UF = '{fullDataSet[7]}',
+            CEP = '{fullDataSet[8]}',
+            TELEFONE = '{fullDataSet[9]}',
+            EMAIL = '{fullDataSet[10]}',
+
+            WHERE CNPJ = '{fullDataSet[0]}'""")
+
+        self.conection.commit()
---
+++ ui_funtions.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+def consulta_cnpj(cnpj):
+
+    url = f"https://www.receitaws.com.br/v1/cnpj/{cnpj}"
+    querystring = {"token":"XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX","cnpj":"06990590000123","plugin":"RF"}
+    response = requests.request("GET", url, params=querystring)
+
+    resp = json.loads(response.text)
+
+
+    return resp['nome'], resp['logradouro'], resp['numero'], resp['complemento'], resp['bairro'], resp['municipio'], resp['uf'], resp['cep'], resp['telefone'], resp['email']
+
+
+
+consulta_cnpj('33291488000102')
